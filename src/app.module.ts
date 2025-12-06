45999de17e772e41e0942cac1ea1d717638b178e
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-12-06 12:00:18 +0100

    feat: add payments and users modules with TypeORM integration
    - Added PaymentsModule and UsersModule for handling transactions and user data.
    - Created Transaction entity with fields for id, reference, amount, status, user relation, and createdAt timestamp.
    - Created User entity with fields for id, email, name, googleId, profilePicture, and a relation to transactions.
    - Integrated TypeORM for PostgreSQL database connection using environment variables.
    - Added necessary dependencies in package.json for NestJS, TypeORM, and Passport.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeORMError } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+
+        autoLoadEntities: true,
+        synchronize: true, // Note: Set to false in production
+      }),
+      inject: [ConfigService],
+    }),
+
+    UsersModule,
+
+    PaymentsModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
