13c08b3030f2bf71aaf8012f70f908224bb4d840
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-12-07 20:26:46 +0100

    refactor: clean up app module by removing unused imports
    - Removed unused AppController, AppService, and TypeORMError imports from app.module.ts to streamline the module structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeORMError } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { PaymentsModule } from './payments/payments.module';
